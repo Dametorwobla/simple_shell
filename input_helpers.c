acbf15ca5fb8e342b18e93e3bb7d726e13991b3a
Henok Gashaw <65940650+Henok100@users.noreply.github.com>
2021-08-25 12:22:55 +0300

    Update input_helpers.c

--- input_helpers.c
+++ input_helpers.c
@@ -19,6 +19,7 @@ char *get_args(char *line, int *exe_ret)
 	size_t n = 0;
 	ssize_t read;
 	char *prompt = "$ ";
+	int hist;
 
 	if (line)
 		free(line);
@@ -111,6 +112,7 @@ int run_args(char **args, char **front, int *exe_ret)
 {
 	int ret, i;
 	int (*builtin)(char **args, char **front);
+	int hist;
 
 	builtin = get_builtin(args[0]);
 
